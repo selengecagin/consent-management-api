d004aa25593de90a03f431e2e04b004f124c9f30
huriselengecagin <selengecagin@gmail.com>
2026-02-12 19:10:39 +0300

    create db object

--- models.py
+++ models.py
@@ -1,2 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+# create db object, no app yet
+# db = SQLAlchemy() -> This creates the SQLAlchemy database object, but without connecting it to a Flask app yet.
+db = SQLAlchemy()
